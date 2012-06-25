codeaurora-unoffical/platform-hardware-ril

RIL: Fix RIL_REQUEST_WRITE_SMS_TO_SIM to return response
SmManager.copyMessageToIccEf() is synchrozied function meaning,
it waits for response from RIL.  In case of invalid arguments to
the function, it returns RIL_E_GENERIC_FAILURE to satisfy
synchronization in telephony frameworks.

Change-Id: I2c14f32bd5ce4a778e1492b027b3ed895ef575e2
CRs-fixed: 347811

libril/ril.cpp
@@ -708,7 +708,12 @@ dispatchSmsWrite (Parcel &p, RequestInfo *pRI) {
 
     args.pdu = strdupReadString(p);
 
-    if (status != NO_ERROR || args.pdu == NULL) {
+    if (status != NO_ERROR ) {
+        LOGE("dispatchSmsWrite: status= %d, args.status= %d", status, args.status);
+        goto invalid;
+    }
+    if( NULL == args.pdu ) {
+        LOGE("dispatchSmsWrite: args.pdu is NULL");
         goto invalid;
     }
 
@@ -735,6 +740,7 @@ dispatchSmsWrite (Parcel &p, RequestInfo *pRI) {
     return;
 invalid:
     invalidCommandBlock(pRI);
+    RIL_onRequestComplete(pRI, RIL_E_GENERIC_FAILURE, NULL, 0);
     return;
 }
 

jellybean4DX/platform_hardware_ril

Change-Id: I8a95cc20bde3b02dc630368fe835258e648612b0

libril/ril.cpp
@@ -533,7 +533,12 @@ dispatchSmsWrite (Parcel &p, RequestInfo *pRI) {
 
     args.pdu = strdupReadString(p);
 
-    if (status != NO_ERROR || args.pdu == NULL) {
+    if (status != NO_ERROR ) {
+        ALOGE("dispatchSmsWrite: status= %d, args.status= %d", status, args.status);
+        goto invalid;
+    }
+    if( NULL == args.pdu ) {
+        ALOGE("dispatchSmsWrite: args.pdu is NULL");
         goto invalid;
     }
 
@@ -560,6 +565,7 @@ dispatchSmsWrite (Parcel &p, RequestInfo *pRI) {
     return;
 invalid:
     invalidCommandBlock(pRI);
+    RIL_onRequestComplete(pRI, RIL_E_GENERIC_FAILURE, NULL, 0);
     return;
 }
 